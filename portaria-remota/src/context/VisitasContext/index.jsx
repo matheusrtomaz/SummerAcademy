ac0eb205bdf1fa6712f5f2843138f9f69cf848c0
Matheus Rodrigues Tomaz <157989074+matheusrtomaz@users.noreply.github.com>
2025-01-30 15:32:11 -0300

    LocalStorage

--- portaria-remota/src/context/VisitasContext/index.jsx
+++ portaria-remota/src/context/VisitasContext/index.jsx
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const VisitasContext = createContext();
 
 export const VisitasProvider = ({ children }) => {
-  const [visitas, setVisitas] = useState([]);
+  // Carrega as visitas do LocalStorage ao inicializar
+  const [visitas, setVisitas] = useState(() => {
+    const visitasSalvas = localStorage.getItem('visitas');
+    return visitasSalvas ? JSON.parse(visitasSalvas) : [];
+  });
+
+  // Salva as visitas no LocalStorage sempre que o estado é atualizado
+  useEffect(() => {
+    localStorage.setItem('visitas', JSON.stringify(visitas));
+  }, [visitas]);
 
   const adicionarVisita = (novaVisita) => {
     setVisitas([...visitas, novaVisita]);
@@ -15,41 +24,3 @@ export const VisitasProvider = ({ children }) => {
     </VisitasContext.Provider>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { createContext, useState } from 'react';
-
-// export const VisitasContext = createContext();
-
-// export const VisitasProvider = ({ children }) => {
-//     const [visitas, setVisitas] = useState([]);
-
-//     const adicionarVisita = (novaVisita) => {
-//         setVisitas([...visitas, novaVisita]);
-//     };
-
-//     return (
-//         <VisitasContext.Provider value={{ visitas, adicionarVisita }}>
-//             {children}
-//         </VisitasContext.Provider>
-//     );
-// };
